04af1a3007432cfc44e3a69f30ef105be3b98d2d
moi-si <111831296+moi-si@users.noreply.github.com>
2026-01-03 10:01:54 +0800

    v0.2.1

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ import (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintln(os.Stderr, "moi-si/lumine v0.2.0")
+		fmt.Fprintln(os.Stderr, "moi-si/lumine v0.2.1")
 		fmt.Fprintln(os.Stderr)
 		flag.PrintDefaults()
 	}
