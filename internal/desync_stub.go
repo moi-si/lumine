4ec8905988082b28d7c13696052b227679169862
moi-si <111831296+moi-si@users.noreply.github.com>
2026-03-28 17:13:56 +0800

    desync: use custom error instead of errors.ErrUnupported

--- internal/desync_stub.go
+++ internal/desync_stub.go
@@ -1,4 +1,4 @@
-//go:build darwin
+//go:build !windows && !linux
 
 package lumine
 
@@ -8,10 +8,12 @@ import (
 	"time"
 )
 
+var errTTLDNotSupported = errors.New("`ttl-d` is not supported on current system")
+
 func minReachableTTL(string, bool, int, int, time.Duration) (int, bool, error) {
-	return -1, false, errors.ErrUnsupported
+	return -1, false, errTTLDNotSupported
 }
 
 func desyncSend(net.Conn, bool, []byte, int, int, int, time.Duration) error {
-	return errors.ErrUnsupported
+	return errTTLDNotSupported
 }
